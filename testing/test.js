220396cae80621b1cb5cea3577875c39eae163b1
Pengskii09 <yojie9@gmail.com>
2025-03-01 01:26:29 +0800

    updates

--- testing/test.js
+++ testing/test.js
@@ -33,7 +33,7 @@
 // const pokemonSprite = data.sprites.other.showdown.front_default;
 
 
-const pokemonCount = 100;
+const pokemonCount = 20;
 var pokedex = {};
 
 window.onload = async function() {
@@ -55,16 +55,20 @@ async function getPokemon(num) {
     console.log(pokemon);
 
     // Get the Pokémon name.
-    let pokemonName = pokemon.name;
+    // Capitalize the first letter of the Pokémon name.
+    let pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
 
-    // Get the types as a comma-separated string.
-    let pokemonTypes = pokemon.types.map(t => t.type.name);
+    // Get the types.
+    // Capitalize the first letter of each Pokémon type.
+    let pokemonTypes = pokemon.types.map(t => 
+        t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1)
+    );
     
     // Get the default front image.
-    // let pokemonImg = pokemon.sprites.front_default;
+    let pokemonImg = pokemon.sprites.front_default;
 
     // Pokemon Home Sprites
-    let pokemonImg = pokemon.sprites.other.home.front_default;
+    // let pokemonImg = pokemon.sprites.other.home.front_default;
 
     // Pokemon Official Artwork Sprites
     // let pokemonImg = pokemon.sprites.other["official-artwork"].front_default;
@@ -72,8 +76,11 @@ async function getPokemon(num) {
     // Pokemon Gen V Animated Sprites
     // let pokemonImg = pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default;
 
+    // On Hover/ Image 2
+    let pokemonImg2 = pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default;
+
     // Save the data to our pokedex object.
-    pokedex[num] = { "name": pokemonName, "types": pokemonTypes, "img": pokemonImg };
+    pokedex[num] = { "name": pokemonName, "types": pokemonTypes, "img": pokemonImg, "img2": pokemonImg2};
 }
 
 // This function creates a card element for each Pokémon.
@@ -101,6 +108,23 @@ function createCard(pokemon, id) {
             ${typesHTML}
         </div>
     `;
+
+    // When the mouse enters the card, swap the image source.
+    card.addEventListener('mouseenter', () => {
+        const img = card.querySelector('#pokemonSprite');
+        if (img) {
+            img.src = pokemon.img2;
+        }
+    });
+
+    // When the mouse leaves, change it back.
+    card.addEventListener('mouseleave', () => {
+        const img = card.querySelector('#pokemonSprite');
+        if (img) {
+            img.src = pokemon.img;
+        }
+    });
+
     return card;
 }
 
